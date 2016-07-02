486df5a72156ac4bba8bdbf0f975f3c253e9ce6a
Ollo <Ollo@dev1>
2016-07-02 16:59:05 +0200

    Used constants instead of the bare numbers

--- Firmware/src/lcd/spi-implement.c
+++ Firmware/src/lcd/spi-implement.c
@@ -49,18 +49,18 @@ spi_implement_ret_t spi_implement_init(void)
 {
 
   /*
-   * Initializes the SPI driver 2. The SPI2 signals are routed as follow:
+   * Initializes the first SPI driver. The SPI1 signals are routed as follow:
    * PA4 - NSS.
    * PA5 - SCK.
    * PA6 - MISO.
    * PA7 - MOSI.
    */
   spiStart(&SPID1, &spicfg);
-  palSetPad(GPIOA, 4);
-  palSetPadMode(GPIOA, 4, PAL_MODE_OUTPUT_PUSHPULL);        /* NSS.     */
-  palSetPadMode(GPIOA, 5, PAL_MODE_OUTPUT_OPENDRAIN);           /* SCK.     */
-  palSetPadMode(GPIOA, 6, PAL_MODE_INPUT);           /* MISO.    */
-  palSetPadMode(GPIOA, 7, PAL_MODE_OUTPUT_OPENDRAIN);           /* MOSI.    */
+  palSetPad(GPIOA, GPIOA_SPI1_NSS);
+  palSetPadMode(GPIOA, GPIOA_SPI1_NSS, PAL_MODE_OUTPUT_PUSHPULL);        /* NSS.     */
+  palSetPadMode(GPIOA, GPIOA_SPI1_SCK, PAL_MODE_OUTPUT_OPENDRAIN);           /* SCK.     */
+  palSetPadMode(GPIOA, GPIOA_SPI1_MISO, PAL_MODE_INPUT);           /* MISO.    */
+  palSetPadMode(GPIOA, GPIOA_SPI1_MOSI, PAL_MODE_OUTPUT_OPENDRAIN);           /* MOSI.    */
 
    return SPI_IMPL_RET_OK;
 }
