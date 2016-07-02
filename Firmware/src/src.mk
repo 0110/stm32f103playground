4094aec9e7ae226e013fcdf0d7351b7c93caa482
Ollo <Ollo@dev1>
2016-07-02 14:41:04 +0200

    Integrated logic from the STM32F4 Project (aka Wookiecontroller)

--- Firmware/src/src.mk
+++ Firmware/src/src.mk
@@ -1,5 +1,7 @@
 # List of all the board related files.
 APPSRC = src/main.c \
+	 src/lcd/ssd1803a-spi.c \
+         src/lcd/spi-implement.c \
          src/usbcfg.c
 
 # Required include directories
