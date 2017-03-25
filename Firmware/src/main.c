7d5b1d4ec9f835ac24d1d354a35f552cff9fb49f
Ollo <Ollo@dev1>
2017-03-25 01:49:46 +0100

    Migrated more files

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -25,8 +25,138 @@
 
 #include "usbcfg.h"
 
+#include "ff.h"
+
 /* Virtual serial port over USB.*/
 SerialUSBDriver SDU1;
+
+
+/*===========================================================================*/
+/* Card insertion monitor.                                                   */
+/*===========================================================================*/
+
+#define POLLING_INTERVAL                10
+#define POLLING_DELAY                   10
+
+/**
+ * @brief   Card monitor timer.
+ */
+static virtual_timer_t tmr;
+
+/**
+ * @brief   Debounce counter.
+ */
+static unsigned cnt;
+
+/**
+ * @brief   Card event sources.
+ */
+static event_source_t inserted_event, removed_event;
+
+/**
+ * @brief   Insertion monitor timer callback function.
+ *
+ * @param[in] p         pointer to the @p BaseBlockDevice object
+ *
+ * @notapi
+ */
+static void tmrfunc(void *p) {
+  BaseBlockDevice *bbdp = p;
+
+  chSysLockFromISR();
+  if (cnt > 0) {
+    if (blkIsInserted(bbdp)) {
+      if (--cnt == 0) {
+        chEvtBroadcastI(&inserted_event);
+      }
+    }
+    else
+      cnt = POLLING_INTERVAL;
+  }
+  else {
+    if (!blkIsInserted(bbdp)) {
+      cnt = POLLING_INTERVAL;
+      chEvtBroadcastI(&removed_event);
+    }
+  }
+  chVTSetI(&tmr, MS2ST(POLLING_DELAY), tmrfunc, bbdp);
+  chSysUnlockFromISR();
+}
+
+/**
+ * @brief   Polling monitor start.
+ *
+ * @param[in] p         pointer to an object implementing @p BaseBlockDevice
+ *
+ * @notapi
+ */
+static void tmr_init(void *p) {
+
+  chEvtObjectInit(&inserted_event);
+  chEvtObjectInit(&removed_event);
+  chSysLock();
+  cnt = POLLING_INTERVAL;
+  chVTSetI(&tmr, MS2ST(POLLING_DELAY), tmrfunc, p);
+  chSysUnlock();
+}
+
+/*===========================================================================*/
+/* FatFs related.                                                            */
+/*===========================================================================*/
+
+/**
+ * @brief FS object.
+ */
+static FATFS SDC_FS;
+
+/* FS mounted and ready.*/
+static bool fs_ready = FALSE;
+
+/* Maximum speed SPI configuration (18MHz, CPHA=0, CPOL=0, MSb first).*/
+static SPIConfig hs_spicfg = {NULL, IOPORT2, GPIOB_SPI2NSS, 0, 0};
+
+/* Low speed SPI configuration (281.250kHz, CPHA=0, CPOL=0, MSb first).*/
+static SPIConfig ls_spicfg = {NULL, IOPORT2, GPIOB_SPI2NSS,
+                              SPI_CR1_BR_2 | SPI_CR1_BR_1,
+                              0};
+
+/* MMC/SD over SPI driver configuration.*/
+static MMCConfig mmccfg = {&SPID2, &ls_spicfg, &hs_spicfg};
+
+/* Generic large buffer.*/
+static uint8_t fbuff[1024];
+
+static FRESULT scan_files(BaseSequentialStream *chp, char *path) {
+  static FILINFO fno;
+  FRESULT res;
+  DIR dir;
+  size_t i;
+  char *fn;
+
+  res = f_opendir(&dir, path);
+  if (res == FR_OK) {
+    i = strlen(path);
+    while (((res = f_readdir(&dir, &fno)) == FR_OK) && fno.fname[0]) {
+      if (_FS_RPATH && fno.fname[0] == '.')
+        continue;
+      fn = fno.fname;
+      if (fno.fattrib & AM_DIR) {
+        *(path + i) = '/';
+        strcpy(path + i + 1, fn);
+        res = scan_files(chp, path);
+        *(path + i) = '\0';
+        if (res != FR_OK)
+          break;
+      }
+      else {
+        chprintf(chp, "%s/%s\r\n", path, fn);
+      }
+    }
+  }
+  return res;
+}
+
+
 
 /*===========================================================================*/
 /* Command line related.                                                     */
@@ -34,6 +164,33 @@ SerialUSBDriver SDU1;
 
 #define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
 #define TEST_WA_SIZE    THD_WORKING_AREA_SIZE(256)
+
+static void cmd_tree(BaseSequentialStream *chp, int argc, char *argv[]) {
+  FRESULT err;
+  uint32_t clusters;
+  FATFS *fsp;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: tree\r\n");
+    return;
+  }
+  if (!fs_ready) {
+    chprintf(chp, "File System not mounted\r\n");
+    return;
+  }
+  err = f_getfree("/", &clusters, &fsp);
+  if (err != FR_OK) {
+    chprintf(chp, "FS: f_getfree() failed\r\n");
+    return;
+  }
+  chprintf(chp,
+           "FS: %lu free clusters, %lu sectors per cluster, %lu bytes free\r\n",
+           clusters, (uint32_t)SDC_FS.csize,
+           clusters * (uint32_t)SDC_FS.csize * (uint32_t)MMCSD_BLOCK_SIZE);
+  fbuff[0] = 0;
+  scan_files(chp, (char *)fbuff);
+}
 
 static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
   size_t n, size;
@@ -104,6 +261,7 @@ static const ShellCommand commands[] = {
   {"mem", cmd_mem},
   {"threads", cmd_threads},
   {"write", cmd_write},
+  {"tree", cmd_tree},
   {NULL, NULL}
 };
 
@@ -113,7 +271,7 @@ static const ShellConfig shell_cfg1 = {
 };
 
 /*===========================================================================*/
-/* Generic code.                                                             */
+/* Main and Generic code.                                                    */
 /*===========================================================================*/
 
 /*
@@ -134,10 +292,48 @@ static __attribute__((noreturn)) THD_FUNCTION(Thread1, arg) {
 }
 
 /*
+ * Card insertion event.
+ */
+static void InsertHandler(eventid_t id) {
+  FRESULT err;
+
+  (void)id;
+  /*
+   * On insertion SDC initialization and FS mount.
+   */
+  if (mmcConnect(&MMCD1))
+    return;
+
+  err = f_mount(&SDC_FS, "/", 1);
+  if (err != FR_OK) {
+    mmcDisconnect(&MMCD1);
+    return;
+  }
+  fs_ready = TRUE;
+}
+
+/*
+ * Card removal event.
+ */
+static void RemoveHandler(eventid_t id) {
+
+  (void)id;
+  mmcDisconnect(&MMCD1);
+  fs_ready = FALSE;
+}
+
+
+/*
  * Application entry point.
  */
 int __attribute__((noreturn)) main(void) {
   thread_t *shelltp = NULL;
+  static const evhandler_t evhndl[] = {
+    InsertHandler,
+    RemoveHandler,
+    ShellHandler
+  };
+  event_listener_t el0, el1, el2;
 
   /*
    * System initializations.
@@ -171,6 +367,19 @@ int __attribute__((noreturn)) main(void) {
   shellInit();
 
   /*
+   * Initializes the MMC driver to work with SPI2.
+   */
+  palSetPadMode(IOPORT2, GPIOB_SPI2NSS, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(IOPORT2, GPIOB_SPI2NSS);
+  mmcObjectInit(&MMCD1);
+  mmcStart(&MMCD1, &mmccfg);
+
+  /*
+   * Activates the card insertion monitor.
+   */
+  tmr_init(&MMCD1);
+
+  /*
    * Creates the blinker thread.
    */
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
@@ -179,6 +388,8 @@ int __attribute__((noreturn)) main(void) {
    * Normal main() thread activity, in this demo it does nothing except
    * sleeping in a loop and check the button state.
    */
+  chEvtRegister(&inserted_event, &el0, 0);
+  chEvtRegister(&removed_event, &el1, 1);
   while (true) {
     if (!shelltp && (SDU1.config->usbp->state == USB_ACTIVE))
       shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
