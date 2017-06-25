6b47ebb0a50f0d50668f806b6dd2fcca74c7e7ae
ollo <ollo@olloThinkPad>
2017-06-25 20:56:24 +0200

    Added error output messages

--- Firmware/src/cmd/cmd_cat.c
+++ Firmware/src/cmd/cmd_cat.c
@@ -13,31 +13,33 @@
 #include "chprintf.h"
 #include "ff.h"
 
-void
-cmd_cat(BaseSequentialStream *chp, int argc, char *argv[])
-{
-  FIL fp;
-  uint8_t buffer[32];
-  int br;
+void cmd_cat(BaseSequentialStream *chp, int argc, char *argv[]) {
+	FIL fp;
+	uint8_t buffer[32];
+	int br;
 
-  if (argc < 1)
-    return;
+	if (argc < 1) {
+		chprintf(chp, "Usage: cmd <file to read>\r\n");
+		return;
+	}
 
-  if (f_open(&fp, (TCHAR*) *argv, FA_READ) != FR_OK)
-    return;
+	if (f_open(&fp, (TCHAR*) *argv, FA_READ) != FR_OK) {
+		chprintf(chp, "[Cat Error] Failed to read file.\r\n");
+		return;
+	}
 
-  do
-    {
-      if (f_read(&fp, (TCHAR*) buffer, 32, (UINT*) &br) != FR_OK)
-        return;
+	do {
+		if (f_read(&fp, (TCHAR*) buffer, 32, (UINT*) &br) != FR_OK) {
+			chprintf(chp, "[Cat Error] Failed to read data\r\n");
+			return;
+		}
 
-      chSequentialStreamWrite(chp, buffer, br);
-    }
-  while (!f_eof(&fp));
+		chSequentialStreamWrite(chp, buffer, br);
+	} while (!f_eof(&fp));
 
-  f_close(&fp);
+	f_close(&fp);
 
-  chprintf(chp, "\r\n");
+	chprintf(chp, "\r\n");
 }
 
 #endif
